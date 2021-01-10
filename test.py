b8631bf8db8a39784b1ccf0d8533e6e63f5eba56
sandyrok <sandeepreddy.rokkam@gmail.com>
2021-01-10 10:08:02 +0530

    first

---
+++ test.py
@@ -0,0 +1,45 @@
+from spinup import ppo_tf1 as ppo
+import tensorflow as tf
+import gym
+
+import sys, os
+
+
+import inspect
+
+# Importing the libraries
+import os
+import numpy as np
+import gym
+import gym_sloped_terrain.envs.stoch2_pybullet_env as e
+from gym import wrappers
+import time
+import multiprocessing as mp
+from multiprocessing import Process, Pipe
+import argparse
+import math
+#Utils
+from utils.logger import DataLog
+from utils.make_train_plots import make_train_plots_ars
+import random
+#Registering new environments
+from gym.envs.registration import registry, register, make, spec
+
+#Stoch 2 Test imports
+import pybullet as p 
+import numpy as np
+PI = math.pi
+# Setting the Hyper Parameters
+import math
+PI = math.pi
+
+
+register(id='Stoch2-v0',entry_point='gym_sloped_terrain.envs.stoch2_pybullet_env:Stoch2Env', kwargs = {'gait' : 'trot', 'render': True, 'action_dim': 20, 'stairs': 0} )
+
+env_fn = lambda : gym.make('Stoch2-v0')
+
+ac_kwargs = dict(hidden_sizes=[64,64], activation=tf.nn.relu)
+
+logger_kwargs = dict(output_dir='path/to/output_dir', exp_name='experiment_name')
+
+ppo(env_fn=env_fn, ac_kwargs=ac_kwargs, steps_per_epoch=5000, epochs= 60, logger_kwargs=logger_kwargs)
